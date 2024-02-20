a0e8c7ff3b48029bc18c163f587ac5f662aa3781
Jacob Best <bestja210@gmail.com>
2024-02-20 13:34:08 -0500

    Initial Commit

---
+++ SkillUPiOS/SkillUPiOSApp.swift
@@ -0,0 +1,17 @@
+//
+//  SkillUPiOSApp.swift
+//  SkillUPiOS
+//
+//  Created by Jacob Best on 2/20/24.
+//
+
+import SwiftUI
+
+@main
+struct SkillUPiOSApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ SkillUPiOSUITests/SkillUPiOSUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  SkillUPiOSUITestsLaunchTests.swift
+//  SkillUPiOSUITests
+//
+//  Created by Jacob Best on 2/20/24.
+//
+
+import XCTest
+
+final class SkillUPiOSUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
